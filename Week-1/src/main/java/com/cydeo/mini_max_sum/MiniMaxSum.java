a2224f361ea2b58da7c1c7e39e723c91ca8a7c47
Adam Husrew <Adam.Husrew@cydeo.com>
2023-01-04 12:56:25 +0300

    Two prolbem solved

--- Week-1/src/main/java/com/cydeo/mini_max_sum/MiniMaxSum.java
+++ Week-1/src/main/java/com/cydeo/mini_max_sum/MiniMaxSum.java
@@ -1,2 +1,42 @@
-package com.cydeo.mini_max_sum;public class MiniMaxSum {
+package com.cydeo.mini_max_sum;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MiniMaxSum {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void main(String[] args) {
+        Integer [] arr = {5, 5, 5, 5, 5};
+
+        miniMaxSum((List<Integer>) Arrays.asList(arr));
+    }
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        long min = arr.get(0);
+        long max = arr.get(0);
+        long sum = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+            sum += arr.get(i);
+            min = Math.min(min, arr.get(i));
+            max = Math.max(max, arr.get(i));
+        }
+
+
+        System.out.println((sum-max) + " " + (sum-min));
+    }
+
+
+
+
+
+
+
 }
--- Week-1/src/main/java/com/cydeo/plus_minus/PlusMinus.java
+++ Week-1/src/main/java/com/cydeo/plus_minus/PlusMinus.java
@@ -1,2 +1,50 @@
-package com.cydeo.plus_minus;public class PlusMinus {
+package com.cydeo.plus_minus;
+
+import java.util.*;
+
+public class PlusMinus {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void main(String[] args) {
+        Integer [] arr = {-4, 3, -9, 0, 4};
+
+        plusMinus((List<Integer>) Arrays.asList(arr));
+    }
+
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+
+        int positives = 0;
+        int negatives = 0;
+        int zeros = 0;
+
+        for (int i : arr){
+            if (i < 0)
+                negatives++;
+            else if (i == 0)
+                zeros++;
+            else
+                positives++;
+        }
+
+        double ratioPositives = (double) positives/arr.size();
+        double ratioNegatives = (double) negatives/arr.size();
+        double ratioZeros = zeros != 0 ? (double) zeros/arr.size() : 0;
+
+
+
+        System.out.format("%f\n",ratioPositives);
+        System.out.format("%f\n",ratioNegatives);
+        System.out.format("%f\n",ratioZeros);
+
+
+
+    }
+
 }
+
